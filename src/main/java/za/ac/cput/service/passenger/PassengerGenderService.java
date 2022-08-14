7a02314171a8b5602344b0c82aad1e613cf517af
Cassy-16 <cassidynangmo@gmail.com>
2022-08-14 22:38:45 +0200

    done my service, although it is still a work in progress

---
+++ src/main/java/za/ac/cput/service/passenger/PassengerGenderService.java
@@ -0,0 +1,10 @@
+package za.ac.cput.service.passenger;
+
+import za.ac.cput.domain.passenger.PassengerGender;
+import za.ac.cput.service.IService;
+
+import java.util.List;
+
+public interface PassengerGenderService extends IService<PassengerGender,String>{
+
+}
